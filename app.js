0d2849d1a77429a069f73b814d19813befdfffcb
Hom Dahal <homdahal997@gmail.com>
2024-04-22 12:37:32 -0400

    learner object function created for holding information for learner

--- app.js
+++ app.js
@@ -76,6 +76,16 @@ const LearnerSubmissions = [
     }
 ];
 
+// Function to initialize a new learner object
+function initializeLearner(learnerId) {
+    return {
+        id: learnerId, // ID of the learner
+        totalScore: 0, // total score of the learner
+        totalPossible: 0, // total possible points the learner can earn
+        assignments: {}, // object to store the assignments of learner
+    };
+}
+
 
 function getLearnerData(course, ag, submissions) {
     // Check if course id matches with assignment group'S course id
@@ -86,28 +96,34 @@ function getLearnerData(course, ag, submissions) {
     // Initialize learners object
     let learners = {};
     // Iterate over each submission
-    for(let submission of submissions){
+    for (let submission of submissions) {
         // Extrate learnerId, assignmentId and score from learners Submission
         let learnerId = submission.learner_id;
         let assignmentId = submission.assignment_id;
         let score = submission.submission.score;
 
-         // Find the corresponding assignment in the assignment group
+        // Find the corresponding assignment in the assignment group
         let assignment = ag.assignments.find((a) => a.id === assignmentId); // check if id prop of obj 'a' is equal to assignmentId
 
         // check if assignment due date is a future date, if so ignore it.
-        if(new Date(assignment.due_at) > new(Date())){
+        if (new Date(assignment.due_at) > new (Date())) {
             continue;
         }
 
         // check and deduct 10% if submission is late
-        if(new Date(submissions.submission.submitted_at) > new Date(assignment.due_at)){
+        if (new Date(submissions.submission.submitted_at) > new Date(assignment.due_at)) {
             score -= score * 0.1;
+        }
+
+        // If learner with the given learner Id not present,
+        // initialize a new learner object and add to learners object.
+        if (!learners[learnerId]) {
+            learners[learnerId] = initializeLearner(learnerId);
         }
 
 
 
-    }// end getLearnerData Function
+    }
     // here, we would process this data to achieve the desired result.
     // const result = [
     //     {
@@ -125,7 +141,7 @@ function getLearnerData(course, ag, submissions) {
     // ];
 
     // return result;
-}
+}// end getLearnerData Function
 
 const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
 
